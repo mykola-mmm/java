0ec504f36fd03bf3e14bea9456318594b1cbdf1e
mykola-mmm <mykola.mazniuk@gmail.com>
2024-06-11 16:07:40 +0200

    java bootcamp

---
+++ src/main/java/org/example/tools/Main.java
@@ -0,0 +1,13 @@
+package org.example.tools;
+
+import org.example.tools.Toolbox.Toolbox;
+
+public class Main {
+    public static void main(String[] args) {
+//        Toolbox toolbox = new Toolbox();
+        String s = "hi";
+        String s2 = s.concat(" ad");
+        System.out.println(s2);
+    }
+
+}
---
+++ src/main/java/org/example/tools/Toolbox/Toolbox.java
@@ -0,0 +1,4 @@
+package org.example.tools.Toolbox;
+
+public class Toolbox {
+}
